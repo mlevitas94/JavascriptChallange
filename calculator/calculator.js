1e94ed9e490f5576d8fe46f5fc6a0d73032ab34f
mlevitas94 <levitasmc@gmail.com>
2020-10-12 13:14:45 -0400

    equations and decimals rounding to nearest 100 thousandth

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,47 @@
 let result = 0
 
-let currentEq = '0'
+let currentEq = ''
+
+let resultText = document.querySelector('.calculatorCont .finalResult')
 
 let currentEqText = document.querySelector('.calculatorCont .currentEq')
 
 
+const calculate = () => {
+    return  Math.round(100000*eval(currentEq))/100000
+}
+
+document.querySelectorAll('.calculator .buttons span').forEach(button => {
+    
+        button.addEventListener('click', () => {
+            if(button.innerHTML === '='){
+                result = calculate()
+                resultText.innerHTML = result
+            }else if(button.innerHTML === 'C'){
+                result = 0
+                currentEq = ''
+                resultText.innerHTML = '0'
+                currentEqText.innerHTML = '0'
+            }else{
+                if(button.innerHTML === '+' || button.innerHTML === '-' || button.innerHTML === '*' || button.innerHTML === '/'){
+                    if(currentEq.includes('+') || currentEq.includes('-') || currentEq.includes('*') || currentEq.includes('/')){
+                        result = calculate()
+                        resultText.innerHTML = result
+                        currentEq = `${calculate()} ${button.innerHTML} `
+                        currentEqText.innerHTML = currentEq
+                        return
+                    }
+                    currentEq += ` ${button.innerHTML} `
+        
+                    currentEqText.innerHTML = currentEq
+    
+                }else{
+                    currentEq += `${button.innerHTML}`
+        
+                    currentEqText.innerHTML = currentEq
+                }
+            }
+        })
+
+})
 
