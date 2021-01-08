d0ed53b527b9491a5410436aa45740fde1fb596f
mlevitas94 <levitasmc@gmail.com>
2021-01-08 17:42:15 -0500

    scrollspy working

--- landing.js
+++ landing.js
@@ -1,14 +1,29 @@
 const navItems = document.querySelectorAll('.sideNav li')
-const projects = document.querySelectorAll('.projectCont')
+const projects = document.querySelectorAll('.projectSection')
+
+const isScrolledIntoViewPartial = (el) => {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+    var isVisible = (elemTop >= -el.clientHeight + 150) && (elemBottom <= window.innerHeight + el.clientHeight - 150);
+    return isVisible;
+  }
 
 window.addEventListener('scroll', () => {
-    console.log('hi')
+    projects.forEach(project => {
+        if(isScrolledIntoViewPartial(project)){
+            navItems.forEach(item => {
+                item.classList.remove('navActive')
+                if(project.id === item.classList[0]){
+                    item.classList.add('navActive')
+                }
+            })
+        }
+    })
 })
 
-const projectDirect = (self, container) => {
-    document.querySelectorAll('.sideNav li').forEach(navItem => {
-        navItem.classList.remove('navActive')
-    })
-    window.scrollBy({ top: document.querySelector(`.${container}`).getBoundingClientRect().y - 100, behavior: 'smooth' }) 
-    self.classList.add('navActive')
+
+
+const projectDirect = (container) => {
+    window.scrollBy({ top: document.querySelector(`.${container}`).getBoundingClientRect().y - 100, behavior: 'smooth' })
 }
